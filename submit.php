c6b02229c5909ec1469b64dd8c5f4a16e375469d
alokreddy31 <alokreddy31@gmail.com>
2018-08-24 05:52:14 -0700

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,13 @@
+ <?php
+
+
+
+
+
+
+    $sql = "INSERT INTO plans (pname,mrental,callcharges,smscharges,datacharges,roamingcharges,freeinternet,freesms,freecalls)
+    VALUES ('".$_POST["pname"]."','".$_POST["mrental"]."','".$_POST["callcharges"]."','".$_POST["smscharges"]."','".$_POST["datacharges"]."','".$_POST["roamingcharges"]."','".$_POST["freeinternet"]."','".$_POST["freesms"]."','".$_POST["freecalls"]."')";
+
+    $result = mysqli_query($conn,$sql);
+}
+ ?>
